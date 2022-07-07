2082f49a7f6e4a00d70b5afab6a9ff56950e3892
Omar Hatem <omarhatem2002@aucegypt.edu>
2022-07-07 15:28:27 +0200

    adds scrolling

--- js/app.js
+++ js/app.js
@@ -39,6 +39,36 @@
 */
 
 // build the nav
+const navBar = document.getElementById("navbar__list");
+const sectionNames = document.querySelectorAll(".sectionsList");
+
+for (let i=0; i<sectionNames.length; i++) {
+    const listItem = document.createElement('li');
+    const listAnchor = document.createElement('a');
+    const listContent = document.createTextNode(sectionNames[i].getAttribute('data-nav'));
+    
+    listAnchor.appendChild(listContent);
+    listAnchor.classList.add("menu__link");
+    listAnchor.setAttribute("href", "#"+sectionNames[i].id);
+    listItem.appendChild(listAnchor);
+
+    navBar.appendChild(listItem);
+}
+
+
+const links = document.querySelectorAll(".navbar__menu .menu__link");
+
+for (let i=0; i<links.length; i++) {
+    links[i].addEventListener("click", function (event) {
+        event.preventDefault();
+        //console.log("event cleard");
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior : "smooth"
+        })
+        //console.log("scrolled");
+    });
+}
+
 
 
 // Add class 'active' to section when near top of viewport
